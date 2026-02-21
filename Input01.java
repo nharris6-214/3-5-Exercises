97a029b0fc2ceb0e03eb28c815e7702298afcf2a
nharris6-214 <nharris66@student.cccs.edu>
2026-02-21 16:57:32 -0700

    Add files via upload

---
+++ Input01.java
@@ -0,0 +1,24 @@
+import javax.swing.JOptionPane;
+
+public class Input01 {
+    public static void main(String[] args) {
+//        //Create a JOptionPane.
+//        //Store the input as a String and print it.
+//
+//        String input = JOptionPane.showInputDialog("Enter number: ");
+//        System.out.println(input);
+//
+//        //Parse the input as an int.
+//        //Print its value +1
+//
+//        int num = Integer.parseInt(input);
+//        System.out.println(++num);
+//
+//        //Try creating a dialog, parsing it, and initializing an int in a single line.
+//        //You should have only one semicolon (;) in this line.
+//
+//        System.out.println(Integer.parseInt(JOptionPane.showInputDialog("Enter number: ")));
+
+        
+    }
+}
